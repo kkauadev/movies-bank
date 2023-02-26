cf12d24b26b8e46d16c551f0b95fc6d1aa160be4
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-02-26 18:48:40 -0300

    changing file names

--- src/core/urls/index.tsx
+++ src/core/urls/index.tsx
@@ -1,4 +1,4 @@
 export const baseURL = "https://api.themoviedb.org/3/";
-export const apiKey = "44c7b8f5dbc120654c2bbbdef102dc19";
-export const languageURL = "&language=pt-BR";
 export const baseURLImg = "https://image.tmdb.org/t/p/original/";
+export const languageURL = import.meta.env.VITE_LANGUAGE;
+export const apiKey = import.meta.env.VITE_TOKEN;
