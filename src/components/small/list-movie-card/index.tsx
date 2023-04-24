e44723d96c9838745483678f8b501441e85c2b42
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-04-24 14:01:41 -0200

    perf: Improved performance of list-movie-card component

--- src/components/small/list-movie-card/index.tsx
+++ src/components/small/list-movie-card/index.tsx
@@ -1,30 +1,34 @@
 import ScrollContainer from "react-indiana-drag-scroll";
 import { MovieItem } from "../../../core/types";
 import { MovieCard } from "../movie-card";
-import { useState } from "react";
+import { memo, useCallback, useState } from "react";
 
 export interface ListMovieCardProps {
   pageTitle: string;
   movieList?: MovieItem[];
 }
 
-export const ListMovieCard = ({ pageTitle, movieList }: ListMovieCardProps) => {
-  const [showMovies, setShowMovies] = useState(false);
+export const ListMovieCard = memo(
+  ({ pageTitle, movieList }: ListMovieCardProps) => {
+    const [showMovies, setShowMovies] = useState(false);
 
-  return (
-    <section className="mb-5">
-      <h2 className="mx-2 mb-1 text-3xl">{pageTitle}</h2>
-      <button
-        className="width-[400px] bg-neutral-600 text-white
+    const handleToggleMovies = useCallback(() => {
+      setShowMovies((prev) => !prev);
+    }, []);
+
+    return (
+      <section className="mb-5">
+        <h2 className="mx-2 mb-1 text-3xl">{pageTitle}</h2>
+        <button
+          className="width-[400px] bg-neutral-600 text-white
         px-4 py-2 rounded mb-2"
-        onClick={() => setShowMovies((prev) => !prev)}
-      >
-        {showMovies ? "Ocultar" : "Mostrar os filmes"}
-      </button>
-      {showMovies && (
-        <ScrollContainer className="scroll-container pb-4 gap-2 flex overflow-x-auto scrollbar-thumb-gray-600 scrollbar-track-white scrollbar-thin scrollbar-custom">
-          {movieList &&
-            movieList.map((movie) => {
+          onClick={handleToggleMovies}
+        >
+          {showMovies ? "Ocultar" : "Mostrar os filmes"}
+        </button>
+        {showMovies && (
+          <ScrollContainer className="scroll-container pb-4 gap-2 flex overflow-x-auto scrollbar-thumb-gray-600 scrollbar-track-white scrollbar-thin scrollbar-custom">
+            {movieList?.map((movie) => {
               return (
                 <MovieCard
                   id={movie.id}
@@ -34,8 +38,9 @@ export const ListMovieCard = ({ pageTitle, movieList }: ListMovieCardProps) => {
                 />
               );
             })}
-        </ScrollContainer>
-      )}
-    </section>
-  );
-};
+          </ScrollContainer>
+        )}
+      </section>
+    );
+  }
+);
